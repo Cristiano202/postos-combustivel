88753d351b63574b722e1917bdf48e5803bd9d3a
Cristiano de carvalho goncalves <20cristianogoncalves20.22@gmail.com>
2026-03-11 18:53:19 -0300

    Primeiro commit - projeto base Spring Boot

---
+++ src/test/java/com/example/postos/PostosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
